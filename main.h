90addefbe3b181c94e2877bf775f2be49039908e
Hagermaher <you@example.com>
2023-09-17 12:54:36 -0700

    update

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
+#ifndef MAIN_H
 #define MAIN_H
-#ifndef MAIM_H
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -7,16 +7,15 @@
 #include <limits.h>
 
 int _putchar(char c);
-int printf_c(va_list args, int y);
-int printf_s(va_list args, int y);
-int printf_i(va_list args, int y);
+int pc(va_list args, int y);
+int ps(va_list args, int y);
+int pi(va_list args, int y);
 int op(const char *format, va_list args, int y);
-int printf_b(unsigned int n, int y);
+int pb(unsigned int n, int y);
 int _printf(const char *format, ...);
-int hexa (unsigned int n, int y, int uppercase);
-int printf_o(unsigned int n, int y);
-int printf_u(unsigned int n, int y);
-int printf_res(va_list args, int y);
-int printf_p(va_list args, int y);
-
+int hexa(unsigned int n, int y, int uppercase);
+int po(unsigned int n, int y);
+int pu(unsigned int n, int y);
+int pr(va_list args, int y);
+int pp(va_list args, int y);
 #endif
