11b2e4501055d83c3697d792a5c098a24eb1eb65
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-18 15:23:24 -0700

    c

---
+++ wu.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * wu - print
+ * @in: arguements
+ * @o: d
+ * @b: characters
+ * @e: f
+ * @a: d
+ * @d: d
+ * @m: s
+ * Return: charcaters
+ */
+
+int wu(int in, int o, char b[], int e, int a, int d, int m)
+{
+	int h;
+	int w;
+	char z;
+
+	h = BUFF_SIZE - o - 1;
+	z = ' ';
+	w = 0;
+	UNUSED(m);
+	UNUSED(in);
+
+	if ((e & ez) && !(e & em))
+		z = 48;
+	if (in)
+		x = 45;
+	else if (e & ep)
+		x = 43;
+	else if (e & es)
+		x = 32;
+	return (wnu(o, b, e, a, d, h, z, x));
+}
