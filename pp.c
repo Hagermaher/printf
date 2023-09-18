ac92baa45aac0c0f24dbbbac5394104a939f35ca
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-09-18 04:54:59 -0700

    c

---
+++ pp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+#define mhd 16
+
+/**
+ * pp - prints
+ * @args: arguements
+ * @y: the characters
+ * Return: charcaters
+ */
+
+int pp(va_list args, int y)
+{
+	void *p = va_arg(args, void*);
+	unsigned long num = (unsigned long) p;
+	int r;
+	int w;
+	unsigned long m;
+	char h[mhd] = "0123456789abcdef";
+	char hd[mhd];
+
+	m = n;
+	r = 0;
+
+	while (m != 0)
+	{
+		r = r + 1;
+		m = m / 16;
+	}
+
+	y += _putchar('0');
+	y += _putchar('x');
+
+	if (n == 0)
+	{
+		y += _putchar('0');
+	}
+	else
+	{
+		for (w = r - 1; w >= 0; w--)
+		{
+			int r = n % 16;
+
+			h[w] = hd[r];
+			n = n / 16;
+		}
+		for (w = 0; w < r; w++)
+		{
+			y += _putchar(h[w]);
+		}
+	}
+	return (y);
+}
