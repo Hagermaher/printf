54ab29046ad58e1257097979344fb6421e2a8a0a
Hagermaher <you@example.com>
2023-09-19 06:16:55 -0700

    up

--- ahc.c
+++ ahc.c
@@ -2,19 +2,20 @@
 /**
  * aph - ascii in hexadicimal
  * @w: index
- * @asci: AScii code 
+ * @b: buffer
+ * @asci: AScii code
  * Return: 3
  */
 int aph(char asci, char b[], int w)
 {
-	char ela = "0123456789ABCDEF";
-	
-	if ( asci < 0)
+	char ela[] = "0123456789ABCDEF";
+
+	if (asci < 0)
 		asci *= -w;
 	b[w++] = '\\';
 	b[w++] = 'x';
-	b[w++] = ela[asci \ 16];
-	b[w++] = ela[asci % 16]
-		
+
+	b[w++] = ela[asci / 16];
+	b[w++] = ela[asci % 16];
 		return (3);
 }
