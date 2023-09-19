13c7f04fcc18ea002ebe6859e5a2a3d8ee7617d9
Hagermaher <you@example.com>
2023-09-18 18:21:13 -0700

    up

---
+++ csn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * csn - cast number
+ * @n: number
+ * @m: size
+ * Return: casted value
+ */
+long int csn(long int n, int m)
+{
+	if (m == ml)
+		return (n);
+	else if (m == ms)
+		return ((short)n);
+
+	return ((int)n);
+}
