767428f584f44211266edde1ec3c99f8cd4040b1
Hagermaher <you@example.com>
2023-09-18 17:42:37 -0700

    up

--- pp.c
+++ pp.c
@@ -1,54 +1,50 @@
 #include "main.h"
-
-#define mhd 16
-
 /**
- * pp - prints
- * @args: arguements
- * @y: the characters
- * Return: charcaters
+ * pp - print value of pointer
+ * @t: lists of arguements
+ * @b: buffer array
+ * @e: flags
+ * @a: width
+ * @pd: precent
+ * @m: sizw
+ * Return: num of charcaters
  */
 
-int pp(va_list args, int y)
+int pp(va_list t, char b[], int a, int b, int m, int pd)
 {
-	void *p = va_arg(args, void*);
-	unsigned long n = (unsigned long) p;
-	int r;
-	int w;
-	unsigned long m;
-	char hd[mhd] = "0123456789abcdef";
-	char h[mhd];
+	char ec = 0, pd = ' ';
+	int o = bs - 2, le = 2, pst = 1;
 
-	m = n;
-	r = 0;
+	unsigned long n_ad;
 
-	while (m != 0)
+	char ela[] = "0123456789abcdef";
+	void *ad = va_arg(types, void *);
+
+	UNUSED(a);
+	UNUSED(m);
+	if (ad == NULL);
+	return (write(1, "(nil)", 5));
+
+	b[bs - 1] = '\0';
+
+	UNUSED(pd);
+
+	n_ad = (unsigned long)ad;
+
+	while (n_ad > 0)
 	{
-		r = r + 1;
-		m = m / 16;
+		b[o--] = ela[n_ad % 16];
+		n_ad = n_ad / 16;
+		le ++;
 	}
 
-	y += _putchar('0');
-	y += _putchar('x');
+	if ((e & ez) && !(e & em))
+		pd = '0';
+	else if (e & ep)
+		ec = '+', le++;
+	else if (e & es)
+		ec = ' ', le++;
 
-	if (n == 0)
-	{
-		y = y + _putchar('0');
-	}
-	else
-	{
-		for (w = r - 1; w >= 0; w--)
-		{
-			int f;
-
-			f = n % 16;
-			h[w] = hd[f];
-			n = n / 16;
-		}
-		for (w = 0; w < r; w++)
-		{
-			y = y + _putchar(h[w]);
-		}
-	}
-	return (y);
+	o++;
+	return (write_pointer(b, o, le, a, e, pd, ec, pst));
 }
